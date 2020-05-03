29338be362bc53abbbe1a165955841876e2f3f0a
ayoub gharbi <ayoub97ma@gmail.com>
2020-05-03 17:11:05 +0000

    manipulate DOM with javascript

---
+++ home.js
@@ -0,0 +1,14 @@
+console.log('hello');
+// alert('hello this is Qazi');
+
+// How to write a comment inline
+
+// Variables
+var b = 'smoothie';
+console.log(b);
+
+var someNumber = 45;
+console.log(someNumber);
+
+var age = this.prompt('What is your age?');
+document.getElementById('someText').innerHTML = age;  
