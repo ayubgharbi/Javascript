66a1ab544114f5b4c664e6885f1a5ef0dd67c9ba
ayoub gharbi <ayoub97ma@gmail.com>
2020-05-04 23:52:37 +0000

    numbers in javascript

--- home.js
+++ home.js
@@ -10,5 +10,22 @@ console.log(b);
 var someNumber = 45;
 console.log(someNumber);
 
-var age = this.prompt('What is your age?');
-document.getElementById('someText').innerHTML = age;  
+// var age = this.prompt('What is your age?');
+// document.getElementById('someText').innerHTML = age;  
+
+// Numbers in Javascript
+var num1 = 10;
+
+// Increment num1 by 1
+num1++;
+
+// Decremnt num1 by 1 
+num1--;
+console.log(num1);
+
+// Divide, multiply *, remainder %
+console.log(num1 % 6);
+
+// Increment/decrement by 10
+num1 += 10;
+console.log(num1);
